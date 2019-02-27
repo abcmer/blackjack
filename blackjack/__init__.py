3579eb3221d678f2d1a46b17600ab0637b722287
Adam Siemer <14990007+abcmer@users.noreply.github.com>
2019-02-26 19:56:04 -0600

    Add PEP8 formatting using YAPF tool
    https://github.com/google/yapf

--- blackjack/__init__.py
+++ blackjack/__init__.py
@@ -2,18 +2,16 @@ import itertools
 from random import shuffle
 import time
 
+
 class Table(object):
     """Table class."""
-    def __init__(self, min_bet=5, max_bet=100):   
-        self.game_mode = 'debug'     
+
+    def __init__(self, min_bet=5, max_bet=100):
+        self.game_mode = 'debug'
         self.min_bet = min_bet
         self.max_bet = max_bet
         self.players = []
-        self.dealer = Player({
-            'id': 'dealer',
-            'name': 'Max',
-            'chips': 1000000
-        })
+        self.dealer = Player({'id': 'dealer', 'name': 'Max', 'chips': 1000000})
         self.shoe = Shoe(1)
         self.game_on = False
 
@@ -23,16 +21,18 @@ class Table(object):
 
     def remove_player(self, player_id):
         """Remove player."""
-        player_to_remove = [player for player in self.players if player.id == player_id][0]
-        
+        player_to_remove = [
+            player for player in self.players if player.id == player_id
+        ][0]
+
     def start_game(self):
         """Start game."""
         self.game_on = True
-        self.shoe.shuffle()  
+        self.shoe.shuffle()
         while any([player.chips >= self.min_bet for player in self.players]):
-            self.clear_table()                                         
-            self.take_bets()            
-            self.deal_cards()            
+            self.clear_table()
+            self.take_bets()
+            self.deal_cards()
             self.play_hand()
             self.pay_out_and_collect()
 
@@ -43,7 +43,8 @@ class Table(object):
         for player in self.players:
             while True:
                 bet_amt = input(f'{player} how much would you like to bet? ')
-                if not is_valid_bet_amt(bet_amt, self.min_bet, self.max_bet, player.chips):
+                if not is_valid_bet_amt(bet_amt, self.min_bet, self.max_bet,
+                                        player.chips):
                     continue
                 else:
                     break
@@ -66,21 +67,19 @@ class Table(object):
         game_command('Dealing the cards...')
         # sleep(.5 * len(self.players))
         # done_dealing is true when all players and the dealer have 2 cards
-        done_dealing = all([
-            [len(player.hand.cards) == 2 for player in self.players],
-            len(self.dealer.hand.cards) == 2]
-        )
+        done_dealing = all(
+            [[len(player.hand.cards) == 2 for player in self.players],
+             len(self.dealer.hand.cards) == 2])
 
         while not done_dealing:
             for player in self.players:
                 if len(player.hand.cards) < 2:
-                    player.hand.cards.append(self.shoe.get_top_card())              
-            self.dealer.hand.cards.append(self.shoe.get_top_card()) 
+                    player.hand.cards.append(self.shoe.get_top_card())
+            self.dealer.hand.cards.append(self.shoe.get_top_card())
             # reset done_dealing
-            done_dealing = all([
-            [len(player.hand.cards) == 2 for player in self.players],
-            len(self.dealer.hand.cards) == 2]
-            )
+            done_dealing = all(
+                [[len(player.hand.cards) == 2 for player in self.players],
+                 len(self.dealer.hand.cards) == 2])
 
     def play_hand(self):
         """Play Hand."""
@@ -97,18 +96,23 @@ class Table(object):
             if hand.check_for_blackjack():
                 print(f'BLACKJACK FOR {player} with {hand}!!!!')
                 continue
-                        
+
             while hand.status not in ('bust', 'stay', 'dealer_blackjack'):
                 if 'Ace' in [card.rank for card in hand.cards]:
                     total = hand.calculate_total()
-                    print(f'{player} you have {player.hand} for a total of {total} (or {total - 10})')
+                    print(
+                        f'{player} you have {player.hand} for a total of {total} (or {total - 10})'
+                    )
                     game_legend('H=Hit, S=Stay')
                     decision = input('Decision:')
-                    ace = [card for card in hand.cards if card.rank =='Ace'][0]
+                    ace = [card for card in hand.cards
+                           if card.rank == 'Ace'][0]
                     # TODO -- Add logic for changing 11 to 1 if bust
                 else:
                     print('')
-                    print(f'{player} you have {player.hand} for a total of {player.hand.calculate_total()}.')
+                    print(
+                        f'{player} you have {player.hand} for a total of {player.hand.calculate_total()}.'
+                    )
                     print('What would you like to do?')
                     game_legend('H=Hit, S=Stay')
                     decision = input('Decision: ')
@@ -120,12 +124,14 @@ class Table(object):
                         print(f'{player} BUSTS!!!!!')
                     if hand.calculate_total() == 21:
                         print(f'Nice Hit {player}')
-                        print(f'You have {player.hand} for a total of {player.hand.calculate_total()}.')
+                        print(
+                            f'You have {player.hand} for a total of {player.hand.calculate_total()}.'
+                        )
                         hand.set_status('stay')
                 elif decision == 'S':
                     hand.set_status('stay')
                     continue
-                                       
+
         # Process dealers turn
         dealer_hand = self.dealer.hand
         while (dealer_hand.calculate_total() < 17):
@@ -136,40 +142,57 @@ class Table(object):
     def pay_out_and_collect(self):
         """Pay out and collect."""
         dealer_hand = self.dealer.hand
-        print(f'Dealer has {dealer_hand} for a total of {dealer_hand.calculate_total()}')
+        print(
+            f'Dealer has {dealer_hand} for a total of {dealer_hand.calculate_total()}'
+        )
         dealer_total = self.dealer.hand.calculate_total()
 
         # For each player check if they beat dealer
         for player in self.players:
             hand = player.hand
             bet_amt = player.hand.bet_amt
-            
+
             if hand.calculate_total() > 21:
                 player.subtract_chips(bet_amt)
-                print(f'{player} loses {bet_amt} dollars. Total remaining chips: {player.chips}')
+                print(
+                    f'{player} loses {bet_amt} dollars. Total remaining chips: {player.chips}'
+                )
             elif dealer_total > 21:
                 player.add_chips(bet_amt)
-                print(f'{player} wins {bet_amt} dollars. Total remaining chips: {player.chips}')
+                print(
+                    f'{player} wins {bet_amt} dollars. Total remaining chips: {player.chips}'
+                )
             elif (hand.calculate_total() > dealer_total):
                 player.add_chips(bet_amt)
-                print(f'{player} wins {bet_amt} dollars. Total remaining chips: {player.chips}')
-            elif (hand.status == 'blackjack') and (dealer_hand.status == 'blackjack'):
+                print(
+                    f'{player} wins {bet_amt} dollars. Total remaining chips: {player.chips}'
+                )
+            elif (hand.status == 'blackjack') and (
+                    dealer_hand.status == 'blackjack'):
                 print(f'{player} and dealer both have blackjack. PUSH')
             elif (hand.calculate_total() <= dealer_total):
                 player.subtract_chips(bet_amt)
-                print(f'{player} loses {bet_amt} dollars. Total remaining chips: {player.chips}')
+                print(
+                    f'{player} loses {bet_amt} dollars. Total remaining chips: {player.chips}'
+                )
+
 
 class Shoe(object):
     """Shoe class."""
+
     def __init__(self, deck_count=6):
         suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
-        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+        ranks = [
+            '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen',
+            'King', 'Ace'
+        ]
         # Create deck of cards and multiply by deck_count to create shoe
-        self.cards = [Card(rank, suit) for suit in suits for rank in ranks ] * deck_count 
+        self.cards = [Card(rank, suit) for suit in suits
+                      for rank in ranks] * deck_count
         self.discard_pile = []
 
     def shuffle(self):
-        """Shuffle the cards."""   
+        """Shuffle the cards."""
         game_command('Dealer is shuffling...')
         # sleep(3)
         shuffle(self.cards)
@@ -185,13 +208,15 @@ class Shoe(object):
         """Gets top card from the shoe, handles end of shoe."""
         if len(self.cards) > 0:
             return self.cards.pop(0)
-        else: # If no more cards, reset the shoe
+        else:  # If no more cards, reset the shoe
             print('Dealer is resetting the shoe...')
             self.reset()
             return self.get_top_card()
 
+
 class Card(object):
     """Card class."""
+
     def __init__(self, rank, suit):
         if rank in ('Jack', 'Queen', 'King'):
             value = 10
@@ -200,7 +225,7 @@ class Card(object):
         else:
             value = int(rank)
         self.rank = rank
-        self.suit = suit        
+        self.suit = suit
         self.value = value
 
     def __repr__(self):
@@ -209,6 +234,7 @@ class Card(object):
 
 class Player(object):
     """Player class."""
+
     def __init__(self, player_config):
         self.id = player_config['id']
         self.name = player_config['name']
@@ -229,6 +255,7 @@ class Player(object):
 
 class Hand(object):
     """Hand class."""
+
     def __init__(self):
         self.bet_amt = 0
         self.cards = []
@@ -241,31 +268,32 @@ class Hand(object):
         return sum([card.value for card in self.cards])
 
     def set_status(self, status):
-        assert status in ('hit', 'stay','split', 'bust', 'double', 'ace', 'blackjack', 'dealer_blackjack')
+        assert status in ('hit', 'stay', 'split', 'bust', 'double', 'ace',
+                          'blackjack', 'dealer_blackjack')
         self.status = status
 
     def set_outcome(self, outcome):
         assert outcome in ('win', 'lose')
 
-    def check_for_blackjack(self):    
+    def check_for_blackjack(self):
         if (self.calculate_total() == 21) and (len(self.cards) == 2):
             self.status = 'blackjack'
             return True
 
     def hit(self, card):
         self.cards.append(card)
-        self.set_status('hit')        
+        self.set_status('hit')
 
     def check_for_bust(self):
         """Check if total of cards is greater than 21."""
-        if self.calculate_total() > 21:            
+        if self.calculate_total() > 21:
             self.set_status('bust')
             self.set_outcome('lose')
             return True
 
     def set_bet_amt(self, amt):
         """Set current bet to the amount provided."""
-        self.bet_amt = amt    
+        self.bet_amt = amt
 
 
 def is_valid_bet_amt(amt, min_bet, max_bet, chips_available):
@@ -300,7 +328,7 @@ def game_legend(message):
     print('')
 
 
-def game_warning(message):    
+def game_warning(message):
     print(f'*** {message} ***')
     print('')
 
@@ -327,10 +355,4 @@ if __name__ == "__main__":
         player = Player(player_config)
         table.add_player(player)
 
-    table.start_game()    
-
-    
-    
-
-
-    
+    table.start_game()
